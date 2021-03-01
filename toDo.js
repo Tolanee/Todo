3e2906efecba96d7c5182bd544f99e25b785333a
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-03-01 20:31:08 +0100

    another commit

--- toDo.js
+++ toDo.js
@@ -3,6 +3,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 todoButton.addEventListener('click', addTodo)
+todoList.addEventListener('click', deleteCheck);
 
 function addTodo(event){
   event.preventDefault();
@@ -25,17 +26,20 @@ function addTodo(event){
   trashButton.classList.add('trash-btn');
   todoDiv.appendChild(trashButton);
   todoList.appendChild(todoDiv);
-  todoInput.value =""
+  todoInput.value ="";
 }
-
-//look at the left top of your vscode, you'll see terminal click it to open a new terminal? respond if you get
-//just created new terminal
-//inside the terminal do git status
-//just did it. its the same as the picture i sent
-// probably didn't structure your folder well 
-//on the terminal type ls and send a screenshot
-//just 'ls' withput the quotes
-//dope, do git add . now
-//the only solution I can think of now is to change the folder directory. a git process already crash in the folder or so
-//okay try this now git init do git add . now
-// did you put . aafter th add?
+function deleteCheck(e){
+  const item = e.target;
+  //delete todo
+  if(item.classList[0] === "trash-btn"){
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+     todo.addEventListener('transitionend'), function(){
+      todo.remove();
+     }
+  }
+  if(item.classList[0] === "complete-btn"){
+    const todo = item.parentElement;
+    todo.classList.toggle("completed")
+  }
+}
