5a802bd6a5fde79aeb0efca9ad163906594eeda7
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-03-14 15:38:35 +0100

    commit again

--- toDo.js
+++ toDo.js
@@ -1,11 +1,10 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo")
+
 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-filterOption.addEventListener('ckick',filterTodo);
 
 function addTodo(event){
   event.preventDefault();
@@ -44,20 +43,4 @@ function deleteCheck(e){
     const todo = item.parentElement;
     todo.classList.toggle("completed")
   }
-}
-function filterTodo (e){
-  const todos = todoList.childNodes;
-  todo.forEach(function(todo){
-    switch(e.target.value){
-      case "all":
-        todo.style.display = 'flex';
-      break;
-      case "completed":
-        if (todo.classList.contains ('completed')){
-          todo.style.display ='flex';
-        }else{
-          todo.style.display ='none';
-        }
-    }
-  });
 }
